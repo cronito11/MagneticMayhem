3c614581035662e24a25046da346222bdb295c48
Smit Patel <patelsitu2041@gmail.com>
2025-03-10 17:23:18 -0400

    Added sprites to player
    - Attached sprites to players north and south
    - created a script to control the graphics of the player

---
+++ Assets/_Project/Scripts/Player/PlayerGraphicManager.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+namespace MagneticMayhem
+{
+    public class PlayerGraphicManager : MonoBehaviour
+    {
+        [SerializeField] private Sprite north;
+        [SerializeField] private Sprite south;
+
+        private SpriteRenderer spriteRenderer;
+        private Magnetism magnetism;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            magnetism = GetComponentInParent<Magnetism>();
+        }
+        private void OnEnable()
+        {
+            magnetism.SuscribeListener(OnPoleChange);
+        }
+
+        private void OnDisable()
+        {
+            magnetism.RemoveListener(OnPoleChange);
+        }
+        void OnPoleChange(MagnetStatus _newPoleStatus)
+            
+        {
+            if(_newPoleStatus.pole == MagenticPole.positive)
+            {
+                spriteRenderer.sprite = north;
+            }
+            else
+            {
+                spriteRenderer.sprite = south;
+            }
+        }
+    }
+}
